5eb0e3af62a55428abbf80f1d17082340007b9e2
Sasank Sunkara <sunkara@sasank.in>
2015-09-28 02:09:07 +0530

    Updated cachematrix.R with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-##makeVector creates a special "matrix", which is really a list containing a function to
+##makecacheMatrix creates a special "matrix", which is really a list containing a function to
   #set the value of the matrix
   #get the value of the matrix
   #set the value of the inverseMatrix
